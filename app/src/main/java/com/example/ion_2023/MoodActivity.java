7750cb29ec44aebccf888f6c86072daed3706d61
UwU <misterhecker696966@gmail.com>
2023-05-01 23:19:20 +0530

    commit

--- app/src/main/java/com/example/ion_2023/MoodActivity.java
+++ app/src/main/java/com/example/ion_2023/MoodActivity.java
@@ -3,14 +3,20 @@ package com.example.ion_2023;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.animation.Animator;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
+import android.widget.ListView;
 
 import com.airbnb.lottie.LottieAnimationView;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
+
+import java.util.Arrays;
 
 public class MoodActivity extends AppCompatActivity {
 
@@ -19,6 +25,8 @@ public class MoodActivity extends AppCompatActivity {
     private LottieAnimationView angryAnimation;
     private LottieAnimationView calmAnimation;
     private LottieAnimationView lovestruckAnimation;
+    ListView listView;
+    LinearLayout linearLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,12 +38,62 @@ public class MoodActivity extends AppCompatActivity {
         angryAnimation = findViewById(R.id.angry_animation);
         calmAnimation = findViewById(R.id.calm_animation);
         lovestruckAnimation = findViewById(R.id.lovestruck_animation);
+        linearLayout = findViewById(R.id.linearLayoutHide);
+        listView = findViewById(R.id.listViewV);
+        String[] songs = {
+                "Bohemian Rhapsody",
+                "Stairway to Heaven",
+                "Hotel California",
+                "Sweet Child O' Mine",
+                "Back in Black",
+                "Smells Like Teen Spirit",
+                "November Rain",
+                "Imagine",
+                "Billie Jean",
+                "Thriller",
+                "Beat It",
+                "Yesterday",
+                "Let It Be",
+                "Hey Jude",
+                "Don't Stop Believin'",
+                "Livin' on a Prayer",
+                "Sweet Caroline",
+                "I Will Always Love You",
+                "Wonderwall",
+                "The Scientist"
+        };
+
 
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavBar);
         bottomNavigationView.setSelectedItemId(R.id.moodItem);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, songs);
+        listView.setAdapter(adapter);
 
         happyAnimation.setOnClickListener(v -> {
             happyAnimation.playAnimation();
+            happyAnimation.addAnimatorListener(new Animator.AnimatorListener() {
+                @Override
+                public void onAnimationStart(Animator animation) {
+                    // Animation started, do nothing
+                }
+
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    linearLayout.setVisibility(View.GONE);
+                    listView.setVisibility(View.VISIBLE);
+
+                }
+
+                @Override
+                public void onAnimationCancel(Animator animation) {
+                    // Animation cancelled, do nothing
+                }
+
+                @Override
+                public void onAnimationRepeat(Animator animation) {
+                    // Animation repeated, do nothing
+                }
+            });
             sadAnimation.cancelAnimation();
             angryAnimation.cancelAnimation();
             calmAnimation.cancelAnimation();
@@ -43,6 +101,29 @@ public class MoodActivity extends AppCompatActivity {
         });
 
         sadAnimation.setOnClickListener(v -> {
+            sadAnimation.addAnimatorListener(new Animator.AnimatorListener() {
+                @Override
+                public void onAnimationStart(Animator animation) {
+                    // Animation started, do nothing
+                }
+
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    linearLayout.setVisibility(View.GONE);
+                    listView.setVisibility(View.VISIBLE);
+
+                }
+
+                @Override
+                public void onAnimationCancel(Animator animation) {
+                    // Animation cancelled, do nothing
+                }
+
+                @Override
+                public void onAnimationRepeat(Animator animation) {
+                    // Animation repeated, do nothing
+                }
+            });
             happyAnimation.cancelAnimation();
             sadAnimation.playAnimation();
             angryAnimation.cancelAnimation();
@@ -51,6 +132,29 @@ public class MoodActivity extends AppCompatActivity {
         });
 
         angryAnimation.setOnClickListener(v -> {
+            angryAnimation.addAnimatorListener(new Animator.AnimatorListener() {
+                @Override
+                public void onAnimationStart(Animator animation) {
+                    // Animation started, do nothing
+                }
+
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    linearLayout.setVisibility(View.GONE);
+                    listView.setVisibility(View.VISIBLE);
+
+                }
+
+                @Override
+                public void onAnimationCancel(Animator animation) {
+                    // Animation cancelled, do nothing
+                }
+
+                @Override
+                public void onAnimationRepeat(Animator animation) {
+                    // Animation repeated, do nothing
+                }
+            });
             happyAnimation.cancelAnimation();
             sadAnimation.cancelAnimation();
             angryAnimation.playAnimation();
@@ -59,6 +163,29 @@ public class MoodActivity extends AppCompatActivity {
         });
 
         calmAnimation.setOnClickListener(v -> {
+            calmAnimation.addAnimatorListener(new Animator.AnimatorListener() {
+                @Override
+                public void onAnimationStart(Animator animation) {
+                    // Animation started, do nothing
+                }
+
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    linearLayout.setVisibility(View.GONE);
+                    listView.setVisibility(View.VISIBLE);
+
+                }
+
+                @Override
+                public void onAnimationCancel(Animator animation) {
+                    // Animation cancelled, do nothing
+                }
+
+                @Override
+                public void onAnimationRepeat(Animator animation) {
+                    // Animation repeated, do nothing
+                }
+            });
             happyAnimation.cancelAnimation();
             sadAnimation.cancelAnimation();
             angryAnimation.cancelAnimation();
@@ -67,6 +194,29 @@ public class MoodActivity extends AppCompatActivity {
         });
 
         lovestruckAnimation.setOnClickListener(v -> {
+            lovestruckAnimation.addAnimatorListener(new Animator.AnimatorListener() {
+                @Override
+                public void onAnimationStart(Animator animation) {
+                    // Animation started, do nothing
+                }
+
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    linearLayout.setVisibility(View.GONE);
+                    listView.setVisibility(View.VISIBLE);
+
+                }
+
+                @Override
+                public void onAnimationCancel(Animator animation) {
+                    // Animation cancelled, do nothing
+                }
+
+                @Override
+                public void onAnimationRepeat(Animator animation) {
+                    // Animation repeated, do nothing
+                }
+            });
             happyAnimation.cancelAnimation();
             sadAnimation.cancelAnimation();
             angryAnimation.cancelAnimation();
